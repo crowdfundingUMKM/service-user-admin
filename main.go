587b114ea108d5ef49808fefc3913a696a9d955a
ahhzaky <siahmadzaky@gmail.com>
2023-07-16 21:34:09 +0700

    Update /verifyToken to accsess token

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 
 	// can access to get prove token
 	// verify token
-	api.GET("/verify_token", middleware.AuthMiddleware(authService, userAdminService), userHandler.VerifyToken)
+	api.GET("/verifyToken", middleware.AuthMiddleware(authService, userAdminService), userHandler.VerifyToken)
 
 	// Rounting admin
 	api.POST("/email_check", userHandler.CheckEmailAvailability)
