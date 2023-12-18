07422003dd2f21adff09be6d24ff2c83b8e90cf7
ahhzaky <siahmadzaky@gmail.com>
2023-12-19 02:33:27 +0700

    Update path For verify token admin

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 
 	// can access to get prove token
 	// verify token
-	api.GET("/verifyToken", middleware.AuthMiddleware(authService, userAdminService), userHandler.VerifyToken)
+	api.GET("/verifyTokenAdmin", middleware.AuthMiddleware(authService, userAdminService), userHandler.VerifyToken)
 
 	// Rounting admin
 	api.POST("/email_check", userHandler.CheckEmailAvailability)
